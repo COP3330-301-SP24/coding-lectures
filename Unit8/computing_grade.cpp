41c508a1f30a2a3caf3d1b87844045773ee76b3c
CarlosTheran <carlos.theran@upr.edu>
2024-02-20 13:46:42 -0500

    updated

--- Unit8/computing_grade.cpp
+++ Unit8/computing_grade.cpp
@@ -10,22 +10,26 @@ using namespace std;
 
 struct student
 {
-    
+    string student_name;
+    double test_1;
+    double test_2;
+    double test_3;
 };
 
 
 
 void print_menu();
-void test_score(string given_name, string student_name[],double first_test[],double second_test[], double third_test[],int lenght);
-int find_student(string given_name, string student_name[], int lenght);
-void average_score(string given_name, string student_name[],double first_test[],double second_test[], double third_test[],int lenght);
-void compute_all_grades(string student_name[],double first_test[],double second_test[], double third_test[], int length);
+void test_score(string given_name, student list[],int lenght);
+int find_student(string given_name, student list[], int lenght);
+void average_score(string given_name, student list[],int lenght);
+void compute_all_grades(student list[], int length);
 
 
 int main()
 {
-    string student_name[4], given_name;
-    double first_test[4], second_test[4], third_test[4];
+    student student_list[4];
+    string given_name;
+
     int length = 4;
     int option = 0;
     bool sentinel = true;
@@ -43,7 +47,7 @@ int main()
     int index = 0;
     while (!input_file.eof())
     {
-        input_file >> student_name[index] >> first_test[index] >> second_test [index] >> third_test[index];
+        input_file >> student_list[index].student_name >> student_list[index].test_1 >> student_list[index].test_2 >> student_list[index].test_3;
         index++;
     }
    
@@ -60,17 +64,17 @@ int main()
             /* Find the student by the name */
             cout<<"Provide the student's name: ";
             cin >> given_name;
-            test_score(given_name, student_name,first_test,second_test,third_test,length);
+            test_score(given_name, student_list,length);
             break;
         case 2:
             /*Compute student's final grade*/
             cout<<"Provide the student's name: ";
             cin >> given_name;
-            average_score(given_name, student_name,first_test,second_test,third_test,length);
+            average_score(given_name, student_list,length);
             break;
         case 3:
             /*Compute all students final grades.*/
-            compute_all_grades(student_name, first_test, second_test, third_test, length);
+            compute_all_grades(student_list, length);
             break;
         case 4:
             /*quit.*/
@@ -101,14 +105,14 @@ void print_menu()
         cout<<right<<setfill('#')<<setw(40)<<"#"<<endl;
 }
 
-int find_student(string given_name, string student_name[], int lenght)
+int find_student(string given_name, student list[], int lenght)
 {
     //int index=-1;
     int i;
     bool sentinel = true;
     for(i=0;i< lenght; i++)
     {
-        if(given_name == student_name[i])
+        if(given_name == list[i].student_name)
         {
             cout<<given_name<<endl;
             //index = i;
@@ -125,24 +129,24 @@ int find_student(string given_name, string student_name[], int lenght)
     return i;
 }
 
-void test_score(string given_name, string student_name[],double first_test[],double second_test[], double third_test[],int lenght)
+void test_score(string given_name, student list[],int lenght)
 {
-   int index = find_student( given_name, student_name, lenght);
+   int index = find_student( given_name, list, lenght);
    if(index != -1){
-   cout<<student_name[index]<<setw(5)<<"first test: "<< first_test[index] <<" second test: "<< second_test[index] <<" third test: "<< third_test[index]<<endl;}
+   cout<<list[index].student_name<<setw(5)<<"first test: "<< list[index].test_1 <<" second test: "<< list[index].test_2 <<" third test: "<< list[index].test_3<<endl;}
 }
 
-void average_score(string given_name, string student_name[],double first_test[],double second_test[], double third_test[],int lenght)
+void average_score(string given_name, student list[],int lenght)
 {
-    int index = find_student( given_name, student_name, lenght);
+    int index = find_student( given_name, list, lenght);
     if(index != -1){
-    cout<<student_name[index]<<setw(5)<<" Average score: "<< (first_test[index]+second_test[index]+third_test[index])/3<<endl;}
+    cout<<list[index].student_name<<setw(5)<<" Average score: "<< (list[index].test_1+list[index].test_2+list[index].test_3)/3<<endl;}
 }
 
-void compute_all_grades(string student_name[],double first_test[],double second_test[], double third_test[], int length)
+void compute_all_grades(student list[], int length)
 {
     for(int i=0; i<length; i++)
     {
-        cout<< student_name[i] << "-- Final Grade: "<< (first_test[i] + second_test[i] + third_test[i])/3<<endl;
+        cout<< list[i].student_name << "-- Final Grade: "<< (list[i].test_1 + list[i].test_2 + list[i].test_3)/3<<endl;
     }
 }
