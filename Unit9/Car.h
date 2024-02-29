8e552bde9ae323a7b5c7903a6cae8d6da19a2092
CarlosTheran <carlos.theran@upr.edu>
2024-02-29 13:42:54 -0500

    chapter 9

---
+++ Unit9/Car.h
@@ -0,0 +1,32 @@
+#ifndef CLASS_CAR
+#define CLASS_CAR
+
+#include <string>
+
+
+using namespace std;
+
+class Car
+{
+    private:
+        string color;
+        string model;
+        int year;
+        string made;
+    public:
+        Car();
+        ~Car();
+        string get_color();
+        string get_model();
+        int get_year();
+        string get_made();
+
+        void set_color(string col);
+        void set_model(string mod);
+        void set_year(int year);
+        void set_made(string mad);
+
+};
+
+#endif
+
---
+++ implementation.cpp
@@ -0,0 +1,15 @@
+#include "Car.h"
+
+Car::Car(){}
+
+Car::~Car(){}
+
+string Car::get_color()
+{
+    return color;
+}
+
+string Car::get_made()
+{
+    return made;
+}
